9abba40c3a9aff0852abd1042131da33712529f6
Corsinix <99492939+Corsinix@users.noreply.github.com>
2023-05-16 20:55:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function darkmod (){
+    if ($("body").hasClass("dark")){
+      $("body").removeClass("dark");
+
+    }
+    else{
+      $("body").addClass("dark");
+      $("")
+    }
+  };
+  
+  
