8879596d2fda010fb24885a8941013091924a363
Corsinix <corsinilouliano@gmail.com>
2023-05-21 14:46:05 +0200

    +

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 function darkmod (){
+    $("body").toggleClass("dark")
     if ($("body").hasClass("dark")){
-      $("body").removeClass("dark");
-
+      $("#moon").css({display:"block"})
+      $(".darkbutton").css("background-color","#FFFFFF")
+      $("#sun").css({display:"none"})
+      $(".pdp").on("mouseenter",function(){
+        $(".pdp").attr("src","https://media.tenor.com/1y8zDc-ll-EAAAAC/3d-saul-saul-goodman.gif")
+      })
+      $(".pdp").on("mouseleave",function(){
+        $(".pdp").attr("src","pdp.jpg")
+      })
     }
-    else{
-      $("body").addClass("dark");
-      $("")
+    else {
+      $("#moon").css({display:"none"})
+      $(".darkbutton").css("background-color","#1e1b1b")
+      $("#sun").css({display:"block"})
+      $(".pdp").off("mouseenter")
+      $(".pdp").off("mouseleave")
     }
   };
+
+
   
   
