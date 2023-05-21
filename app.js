206e3d0c8e66487b165386786a6378fe3aaf8d69
Corsinix <corsinilouliano@gmail.com>
2023-05-21 16:16:31 +0200

    ++

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 function darkmod (){
     $("body").toggleClass("dark")
     if ($("body").hasClass("dark")){
-      $("#moon").css({display:"block"})
-      $(".darkbutton").css("background-color","#FFFFFF")
-      $("#sun").css({display:"none"})
+      $(".darkbutton").css("background-color","#FFFFFF");
+      $("header").css("background-color","rgb(18, 66, 92)");
+      $("#moon").css({display:"block"});
+      $("#sun").css({display:"none"});
+      $("section").css("background-color","rgb(50, 98, 123)");
+
+
       $(".pdp").on("mouseenter",function(){
         $(".pdp").attr("src","https://media.tenor.com/1y8zDc-ll-EAAAAC/3d-saul-saul-goodman.gif")
-      })
+      });
       $(".pdp").on("mouseleave",function(){
         $(".pdp").attr("src","pdp.jpg")
-      })
+      });
     }
     else {
-      $("#moon").css({display:"none"})
-      $(".darkbutton").css("background-color","#1e1b1b")
-      $("#sun").css({display:"block"})
-      $(".pdp").off("mouseenter")
-      $(".pdp").off("mouseleave")
+      $(".darkbutton").css("background-color","#1e1b1b");
+      $("header").css("background-color","rgb(11, 113, 168)");
+      $("#moon").css({display:"none"});
+      $("#sun").css({display:"block"});
+      $("section").css("background-color","#FFFFFF");
+
+
+      $(".pdp").off("mouseenter");
+      $(".pdp").off("mouseleave");
     }
   };
 
